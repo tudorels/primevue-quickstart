7c35d653dc761ff5b7811c41889c3ef8f50172ef
cagataycivici <cagatay.civici@gmail.com>
2019-05-25 19:17:07 +0300

    Import refactor

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import App from './App.vue';
 
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-import Calendar from 'primevue/components/calendar/calendar.common';
 
 Vue.component('InputText', InputText);
 Vue.component('Button', Button);
-Vue.component('Calendar', Calendar);
 
 import 'primevue/resources/themes/nova-light/theme.css';
 import 'primevue/resources/primevue.min.css';
